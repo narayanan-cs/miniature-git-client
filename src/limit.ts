ee40499cae47ec32c620a2968099b8a5b441f0e4
Narayanan <narayanan.cs.38@gmail.com>
2024-06-25 17:48:59 +0530

    remove comments from test file and delete limit.ts

--- src/limit.ts
+++
@@ -1,8 +0,0 @@
-import { Buffer } from 'node:buffer';
-
-const buf = Buffer.allocUnsafe(100);
-
-//buf.writeInt32BE(1703393292, 0);
-buf.fill('f48dd853820860816c75d54d0f584dc863327a7c',0,20,'hex')
-console.log(buf);
-// Prints: <Buffer 01 02 03 04>
